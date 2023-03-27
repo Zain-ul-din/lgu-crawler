9b380391a44dc9686e6b10436472cfae130e5da3
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-03-28 04:05:27 +0500

    scrap.ts

--- src/scrapper/scrap.ts
+++ src/scrapper/scrap.ts
@@ -1,7 +1,13 @@
+/*
+    NOTE!
+        THIS SCRIPT IS RUN BY GITHUB ACTION TO SCRAP DATA ON THE CLOUD
+*/
+
 import { scrapeMetaData } from "./meta_data";
+import dotenv from "dotenv";
+dotenv.config();
 
 console.log ("scrapping data...");
-console.log ("ID: " + process.env.SESSION_ID);
 
 (async()=>{
     console.log ("MetaData: "+await scrapeMetaData());
