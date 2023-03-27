5a885ce9db812791919cb6422d8c7d69929c87cb
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-03-28 03:44:10 +0500

    data scrapping

--- src/scrapper/scrap.ts
+++ src/scrapper/scrap.ts
@@ -1,6 +1,7 @@
 import { scrapeMetaData } from "./meta_data";
 
 console.log ("scrapping data...");
+console.log ("ID: " + process.env.SESSION_ID);
 
 (async()=>{
     console.log ("MetaData: "+await scrapeMetaData());
