a759e85d1b9f5eae663023b0a0ebd9faacc8fb68
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-11 19:14:03 +0500

    feat: added util functions

---
+++ src/lib/util.ts
@@ -0,0 +1,18 @@
+export async function promisify<T>(values: Promise<T>[]) {
+  return await Promise.all(values);
+}
+
+export function chunkifyArray<T>(arr: T[], chunks: number) {
+  if (chunks <= 0) throw new Error("Number of chunks should be greater than 0");
+
+  const chunkSize = Math.ceil(arr.length / chunks);
+  const res: Array<Array<T>> = [];
+
+  for (let i = 0; i < arr.length; i += chunkSize) res.push(arr.slice(i, i + chunkSize));
+
+  return res;
+}
+
+export function clamp(num: number, min: number, max: number) {
+  return num <= min ? min : num >= max ? max : num;
+}
