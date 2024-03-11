eef5007451228f2f4293a01973f4d7200f51acb0
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-11 19:15:56 +0500

    feat: joined everything

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world')
+import {MetaDataCrawler} from "./crawlers";
+import OptionsParser from "./parsers/OptionsParser";
+import cluster from "cluster";
+import {joinTimetableWorker, spawnTimetablesFetchWorkers} from "./lib/worker";
+
+if (cluster.isPrimary) {
+  const metaDataCrawler = new MetaDataCrawler({parser: new OptionsParser()});
+
+  metaDataCrawler.on("crawl", (data) => {
+    spawnTimetablesFetchWorkers(data.timeTableRequestPayloads);
+  });
+
+  metaDataCrawler.crawl();
+}
+
+joinTimetableWorker((timetableCrawler) => {
+  timetableCrawler.on("crawl", (res) => {
+    console.log(JSON.stringify(res));
+  });
+});
