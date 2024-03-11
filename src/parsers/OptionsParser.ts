566f0dab89e13071cfebc12da270ecc2695d0c3d
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-11 19:15:18 +0500

    added: options parser

---
+++ src/parsers/OptionsParser.ts
@@ -0,0 +1,23 @@
+import {load} from "cheerio";
+import HTMLOptionsType from "../types/HTMLOptionsType";
+import Parser from "./Parser";
+
+class OptionsParser extends Parser<HTMLOptionsType> {
+  public parse(html: string, selector: string): HTMLOptionsType {
+    const $ = load(html);
+    const options = $(selector)
+      .toArray()
+      .map((ele) => ({
+        text: $(ele).text(),
+        value: $(ele).attr("value") || "",
+      }))
+      .filter((opt) => opt.value !== "");
+
+    return {
+      textNodes: options.map((opt) => opt.text.trim()),
+      values: options.map((opt) => opt.value.trim()),
+    };
+  }
+}
+
+export default OptionsParser;
