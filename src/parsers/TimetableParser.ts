846d6e35f539ab00058df417f2ded54c4e25dda4
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-11 19:14:16 +0500

    feat: added parser

---
+++ src/parsers/TimetableParser.ts
@@ -0,0 +1,53 @@
+import {load, Element, CheerioAPI} from "cheerio/lib/slim";
+import Parser from "./Parser";
+
+class TimetableParser extends Parser {
+  public parse(rawContent: string, selector: string = "tr") {
+    const $ = load(rawContent);
+    const rows = $("tr").toArray().slice(1);
+
+    const timetable = rows
+      .map((row) => this.parseRow($, row))
+      .reduce((acc: {}, curr) => {
+        const [entry] = Object.entries(curr);
+        return {...acc, [entry[0]]: entry[1]};
+      }, {});
+    return timetable;
+  }
+
+  private parseRow($: CheerioAPI, row: Element) {
+    const weekName = $(row).find("th").text();
+    const tdNodes = $(row).find("td").toArray();
+
+    const lectures = tdNodes
+      .filter((node) => $(node).text().trim() !== "X" && $(node).text().trim() !== "All slots are free")
+      .map((node) => {
+        const textNodes = $(node)
+          .find("span")
+          .toArray()
+          .map((ele) => $(ele).text().trim());
+        return this.parseTextNodes(textNodes);
+      });
+
+    return {[weekName]: lectures};
+  }
+
+  private parseTextNodes(nodes: string[]) {
+    const time = nodes[4].split("-");
+    return {
+      subject: nodes[0],
+      roomNo: nodes[1],
+      teacher: nodes[2],
+      startTime: {
+        hours: parseInt(time[0].split(":")[0]),
+        minutes: parseInt(time[0].split(":")[1]),
+      },
+      endTime: {
+        hours: parseInt(time[1].split(":")[0]),
+        minutes: parseInt(time[1].split(":")[1]),
+      },
+    };
+  }
+}
+
+export default TimetableParser;
