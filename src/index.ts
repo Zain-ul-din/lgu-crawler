e794bcc62613ea1828e20c5cfb58d5d8c45a9a17
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-13 00:43:47 +0500

    feat: generates sitemaps for SEO

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { hashStr } from "./lib/cipher";
 import {computeRoomTimetable, computeRooms, computeTeacherTimetable, computeTeachers} from "./lib/computes";
 import {writeDB} from "./lib/local-db";
 import Worker from "./lib/worker";
@@ -6,7 +7,7 @@ const worker = new Worker();
 
 worker.onCrawlMetaData(({metaData}) => {
   writeDB("meta_data", metaData, false);
-
+  
   // add more services as needed
 });
 
@@ -17,14 +18,19 @@ worker.onCrawlTimetable((timetable) => {
 });
 
 worker.onFinish((allTimetables) => {
+
+  writeDB("timetable_paths", allTimetables.map(t=> t.uid).map(hashStr), false)
+
   const teachers = computeTeachers(allTimetables);
   writeDB("teachers", teachers, false);
   teachers.forEach((teacher) => writeDB(teacher, computeTeacherTimetable(teacher, allTimetables)));
-
+  writeDB("teacher_paths", teachers.map(hashStr), false)
+  
   const rooms = computeRooms(allTimetables);
   writeDB("rooms", rooms, false);
   rooms.forEach((room) => writeDB(room, computeRoomTimetable(room, allTimetables)));
-
+  writeDB("rooms_paths", rooms.map(hashStr), false)
+  
   // add more services as needed
 });
 
