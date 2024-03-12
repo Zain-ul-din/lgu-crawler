2e19c46738c5240971743d1ece91715f672cbade
Zain_ul_din <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-12 18:01:53 +0000

    feat: create .env file from process.env

---
+++ scripts/env.js
@@ -0,0 +1,19 @@
+/**
+ * use to create .env file if using github codespaces which automatically provides env variables in process.env object
+ */
+
+const {writeFileSync, existsSync} = require("fs");
+const ENV_PATH = process.cwd() + "./.env";
+const DEFAULT_PLACEHOLDER = "YOUR_VALUE";
+
+if (existsSync(ENV_PATH)) process.exit(0);
+
+writeFileSync(
+  `${process.cwd()}/.env`,
+  `
+NODE_ENV=${process.env.NODE_ENV || DEFAULT_PLACEHOLDER}
+OPEN_DB_KEY=${process.env.OPEN_DB_KEY || DEFAULT_PLACEHOLDER}
+OPEN_DB_IV=${process.env.OPEN_DB_IV || DEFAULT_PLACEHOLDER}
+PHPSESSID=${process.env.PHPSESSID || DEFAULT_PLACEHOLDER}
+`
+);
