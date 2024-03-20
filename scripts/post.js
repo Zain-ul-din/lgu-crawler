c721057ef546f2a6c6770303b5af256093f0701b
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-03-20 07:03:12 +0500

    fix: prevented hook URL to being exposed in public logs

--- scripts/post.js
+++
@@ -1,7 +0,0 @@
-const url = process.argv[2];
-
-if(url === undefined) throw new Error('Url not Specified')
-
-fetch(url, {  method: 'post'  }).then(res => res.text())
-.then(res=> console.log(res))
-
---
+++ scripts/vercel-deploy.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const url = process.env.VERCEL_DEPLOYMENT_HOOK || 'https://www.google.com'
+fetch(url, { method: 'POST' }).then(res=> res.text()).then(console.log)
+
